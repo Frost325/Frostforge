bfd8b30b7f4abf038fe9b23c1752b5911758ddc8
Joshua Taylor <smartbrain2005@gmail.com>
2026-06-02 21:22:29 -0400

    Frostforge 0.0.3 -- GameObject Class

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
 import pygame
 
+# CLASSES
+
+# Asset -- image, drawing, etc
+# class Asset: -- BACKBURNER FOR NOW
+
+# GameObject -- anything that renders into play
+class GameObject:
+    def __init__(self, x=0, y=0, assetType=None, image=None, shape="rect", dimensions=(0,0), color=(0,0,0)):
+        self.x = x
+        self.y = y
+        self.assetType = assetType
+        self.image = image
+        self.shape = shape
+        self.dimensions = dimensions
+        self.color = color
+    
+    def render(self):
+        if self.assetType == None:
+            return
+        if self.assetType == "shape": # ADD MORE SHAPES AS NEEDED
+            if self.shape == "rect":
+                pygame.draw.rect(screen, self.color, (self.x, self.y, *self.dimensions))
+            if self.shape == "circle":
+                pygame.draw.circle(screen, self.color, (self.x, self.y), self.dimensions[0])
+        
+        # add here render image, or draw picture
+                
 pygame.init()
 
 # init screen
@@ -9,10 +36,7 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Frostforge")
 
 # rectangle
-rect_x = 100
-rect_y = 100
-rect_width = 50
-rect_height = 50
+player = GameObject(x=100, y=100, assetType="shape", dimensions=(50,50))
 
 running = True
 while running:
@@ -24,24 +48,20 @@ while running:
     # process key presses
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]: # W
-        rect_y -= 5
+        player.y -= 5
     if keys[pygame.K_s]: # S
-        rect_y += 5
+        player.y += 5
     if keys[pygame.K_a]: # A
-        rect_x -= 5
+        player.x -= 5
     if keys[pygame.K_d]: # D
-        rect_x += 5
+        player.x += 5
 
 
     # background
     screen.fill((20, 20, 20))
 
     # draw rect
-    pygame.draw.rect(
-        screen,
-        (128, 0, 255), # purple
-        (rect_x, rect_y, rect_width, rect_height)
-    )
+    player.render()
 
     pygame.display.flip()
 
