0be21128d6cb6ffa741a52cc2caccb5216e1918b
Joshua Taylor <smartbrain2005@gmail.com>
2026-06-03 19:37:53 -0400

    Frostforge 0.0.4 -- Create visual zone with default 10x10 grid layout

--- main.py
+++ main.py
@@ -29,14 +29,42 @@ class GameObject:
                 
 pygame.init()
 
-# init screen
-WIDTH = 800
-HEIGHT = 600
+# COLORS
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GRAY = (20, 20, 20)
+PURPLE = (128, 0, 128)
+
+# LAYOUT SECTIONS
+
+# Screen -- 1920x1080, 1280x720... make it scalable?
+WIDTH = 1280
+HEIGHT = 720
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Frostforge")
 
-# rectangle
+# Visual
+VISUAL_DIM = 620
+VISUAL_BORDER_SIZE = 10
+VISUAL_BACKGROUND_COLOR = WHITE
+VISUAL_BORDER_COLOR = BLACK
+VISUAL_X = 630 # 550 from border
+VISUAL_Y = 70 # 550 from border
+
+# Grid
+GRID_SIZE = 10
+BOX_SIZE = (600 / 10) # THE FIRST BOX IS LINE_WIDTH PIXELS BIGGER BECAUSE THE LINE ISN'T DRAWN INTRUDING INTO COLUMN 1 -- IDEA: MAKE LIST OF CELLS AS RECTS, DRAW EACH CELL, AND DRAW A 1PX BORDER IN THE CELL IT OWNS ITS OWN SPACE, WILL DRAW ALONG BORDER BUT OK
+LINES = []
+LINE_WIDTH = 2
+LINE_COLOR = PURPLE
+for box in range(1, GRID_SIZE):
+    LINES.append(((VISUAL_X + VISUAL_BORDER_SIZE + BOX_SIZE * box, VISUAL_Y + VISUAL_BORDER_SIZE), ((VISUAL_X + VISUAL_BORDER_SIZE + BOX_SIZE * box, VISUAL_Y + VISUAL_DIM - VISUAL_BORDER_SIZE))))
+    LINES.append(((VISUAL_X + VISUAL_BORDER_SIZE, VISUAL_Y + VISUAL_BORDER_SIZE + BOX_SIZE * box), (VISUAL_X + VISUAL_DIM - VISUAL_BORDER_SIZE, VISUAL_Y + VISUAL_BORDER_SIZE + BOX_SIZE * box)))
+
+# objects
+GameObjects = []
 player = GameObject(x=100, y=100, assetType="shape", dimensions=(50,50))
+GameObjects.append(player)
 
 running = True
 while running:
@@ -48,20 +76,31 @@ while running:
     # process key presses
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]: # W
-        player.y -= 5
+        player.y -= 1
     if keys[pygame.K_s]: # S
-        player.y += 5
+        player.y += 1
     if keys[pygame.K_a]: # A
-        player.x -= 5
+        player.x -= 1
     if keys[pygame.K_d]: # D
-        player.x += 5
+        player.x += 1
 
+    # DRAW
 
     # background
-    screen.fill((20, 20, 20))
+    screen.fill(GRAY)
 
-    # draw rect
-    player.render()
+    # visual
+    VISUAL = pygame.Rect(VISUAL_X, VISUAL_Y, VISUAL_DIM, VISUAL_DIM)
+    pygame.draw.rect(screen, VISUAL_BACKGROUND_COLOR, VISUAL)
+    pygame.draw.rect(screen, VISUAL_BORDER_COLOR, VISUAL, VISUAL_BORDER_SIZE)
+
+    # grid
+    for line in LINES:
+        pygame.draw.line(screen, LINE_COLOR, line[0], line[1], LINE_WIDTH)
+
+    # game objects
+    for object in GameObjects:
+        object.render()
 
     pygame.display.flip()
 
